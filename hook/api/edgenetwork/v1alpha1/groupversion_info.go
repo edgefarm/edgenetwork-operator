98a7a0b6e5f35255f6794e41547a03daa8486992
Matthias Mahler <matthias.mahler@ci4rail.com>
2023-01-26 09:00:46 +0100

    added typoes for manifests and generator

---
+++ hook/api/edgenetwork/v1alpha1/groupversion_info.go
@@ -0,0 +1,21 @@
+// Package v1alpha1 contains the v1alpha1 group Stream resources of the NATS provider.
+// +kubebuilder:object:generate=true
+// +groupName=network.edgefarm.io
+// +versionName=v1alpha1
+package v1alpha1
+
+// Package type metadata.
+// const (
+// 	Group   = "network.edgefarm.io"
+// 	Version = "v1alpha1"
+// )
+
+// var (
+// 	// SchemeGroupVersion is group version used to register these objects
+// 	SchemeGroupVersion = schema.GroupVersion{Group: Group, Version: Version}
+
+// 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+// 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+// 	AddToScheme = SchemeBuilder.AddToScheme
+// )
